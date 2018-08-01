6465d3707dd8ff24ddedeff338da4b73665bdff6
ChathuraLakshitha <clakshitha191@gmail.com>
2018-08-01 16:12:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/zeonit/myapplication2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.zeonit.myapplication2;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        LocaleHelper.setLocale(MainActivity.this,"si");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
